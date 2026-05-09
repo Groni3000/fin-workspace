56fe43c587443543641d945168bf2d20a4e463e7
Groni3000 <groni30001@gmail.com>
2026-05-10 01:01:05 +0300

    Update asset.rs

--- instrid/src/asset.rs
+++ instrid/src/asset.rs
@@ -1,11 +1,26 @@
-#[derive(Debug)]
+/// Represents an asset (e.g., equity, commodity, currency, etc.).
+///
+/// Asset - an entity that can be traded or cash settled.
+///
+/// Examples:
+///     - AAPL, Equity
+///     - USD, Currency
+///     - BTC, Currency
+///     - S&P 500, Index (cash settled)
+#[derive(Debug, PartialEq, Eq)]
 pub struct Asset {
-    bytes: [u8; 8],
-    len: u8,
+    name: &'static str,
     category: AssetClass,
 }
 
-#[derive(Debug)]
+impl Asset {
+    pub const fn new(name: &'static str, category: AssetClass) -> Self {
+        Self { name, category }
+    }
+}
+
+/// Represents the class or type of an asset (e.g., equity, commodity, currency, etc.).
+#[derive(Debug, PartialEq, Eq)]
 pub enum AssetClass {
     /// Shares, ETFs, REITs - ownership stakes in companies or funds.
     Equity,
@@ -22,4 +37,19 @@ pub enum AssetClass {
     ///Market indices: S&P 500, NASDAQ Composite, VIX. Not directly tradeable,
     ///but derivatives reference them.
     Index,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_asset_new() {
+        let name = "AAPL";
+        let asset_class = AssetClass::Equity;
+        let aapl = Asset::new(name, asset_class);
+        dbg!(&aapl);
+        assert_eq!(&aapl.name, &name);
+        assert_eq!(&aapl.category, &AssetClass::Equity);
+    }
 }
