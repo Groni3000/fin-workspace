136a80ccb42dfa7968b621d59b45df7cec35fcae
Groni3000 <groni30001@gmail.com>
2026-05-11 21:28:46 +0300

    added display

--- instrid/src/instruments/futures.rs
+++ instrid/src/instruments/futures.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::asset::Asset;
 use crate::instrument::BaseInstrument;
 use crate::mic::Mic;
@@ -44,5 +46,64 @@ impl BaseInstrument for Futures {
 
     fn mic(&self) -> &Mic {
         &self.mic
+    }
+}
+
+impl Display for Futures {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Futures:{}/{}@{} {:04}-{:02}",
+            self.base,
+            self.quote,
+            self.mic,
+            self.year,
+            self.tenor.ordinal()
+        )?;
+
+        if let Some(day) = self.day {
+            write!(f, "-{:02}", day)?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::asset::AssetClass;
+
+    use super::*;
+
+    #[test]
+    fn display_without_day() {
+        let f = Futures::new(
+            Asset::new("CL", AssetClass::Commodity),
+            Asset::new("USD", AssetClass::Currency),
+            Mic::xnas(),
+            2026,
+            Tenor::June,
+            None,
+        );
+        assert_eq!(
+            f.to_string(),
+            "Futures:(Commodity)CL/(Currency)USD@XNAS 2026-06",
+        );
+    }
+
+    #[test]
+    fn display_with_day() {
+        let f = Futures::new(
+            Asset::new("CL", AssetClass::Commodity),
+            Asset::new("USD", AssetClass::Currency),
+            Mic::xnas(),
+            2026,
+            Tenor::June,
+            Some(20),
+        );
+        assert_eq!(
+            f.to_string(),
+            "Futures:(Commodity)CL/(Currency)USD@XNAS 2026-06-20",
+        );
     }
 }
